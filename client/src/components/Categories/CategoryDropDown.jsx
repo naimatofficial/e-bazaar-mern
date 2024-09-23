c82f3074f408ef1805b963bf5fd9eab92d2675bb
Zarak Khan <119649492+Zarakkhan-dev@users.noreply.github.com>
2024-09-23 16:02:01 +0500

    Fixing the Menu bar

--- client/src/components/Categories/CategoryDropDown.jsx
+++ client/src/components/Categories/CategoryDropDown.jsx
@@ -1,59 +1,134 @@
-import { Typography } from '@material-tailwind/react'
-import { useGetCategoriesQuery } from '../../redux/slices/categoriesApiSlice'
-import Loader from '../Loader'
-import { Link } from 'react-router-dom'
-import { capitalizeFirstLetter } from '../../utils'
-const CategoryDropDown = () => {
-    const { data: categories, isLoading } = useGetCategoriesQuery({})
+import { useGetCategoriesQuery } from '../../redux/slices/categoriesApiSlice';
+import Loader from '../Loader';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { capitalizeFirstLetter } from '../../utils';
 
-    console.log(categories)
+const CategoryDropDown = () => {
+    const { data: categories, isLoading } = useGetCategoriesQuery({});
+    const [hoveredCategory, setHoveredCategory] = useState(null);
+    const [hoveredSubCategory, setHoveredSubCategory] = useState(null);
+
+    const handleMouseEnterCategory = (categoryId) => {
+        setHoveredCategory(categoryId);
+    };
+
+    const handleMouseEnterSubCategory = (subCategoryId) => {
+        setHoveredSubCategory(subCategoryId);
+    };
+
+    const handleMouseLeave = () => {
+        setHoveredCategory(null);
+        setHoveredSubCategory(null);
+    };
 
     return isLoading ? (
         <Loader />
-    ) : categories && categories?.doc ? (
-        <>
-            <div className="w-[250px] p-2 mx-auto shadow-md bg-white-100">
-                <div>
-                    {categories.doc.map((category, index) => {
-                        if (index <= 6)
-                            return (
-                                <>
-                                    {' '}
-                                    <Link
-                                        key={index}
-                                        to={`/products?category=${category._id}`}
-                                        className="flex  items-center group  p-1 border-b last:border-none cursor-pointer  gap-3 px-3"
+    ) : (
+        <div className="relative z-20">
+            {categories && categories.doc ? (
+                <div className="w-full p-2 mx-auto shadow-md bg-white">
+                    {categories.doc.slice(0, 7).map((category, index) => {
+                        const hasSubCategories = category.subCategories?.length > 0;
+
+                        return (
+                            <div
+                                key={index}
+                                className="relative group border-b-2 border-gray-200"
+                                onMouseEnter={() => handleMouseEnterCategory(category._id)}
+                            >
+                                <Link
+                                    to={`/products?category=${category._id}`}
+                                    className={`flex items-center group gap-2 p-2 
+                                    cursor-pointer w-full hover:bg-gray-100`}
+                                >
+                                    <div className="image">
+                                        <img
+                                            src="https://vistamart.biz/storage/app/public/category/2024-08-08-66b4dde53db3a.png"
+                                            alt="Logo"
+                                            className="w-[1.5vw] h-[1vw]"
+                                        />
+                                    </div>
+                                    <span className="text-gray-700 group-hover:text-primary-600 flex justify-between w-full">
+                                        {capitalizeFirstLetter(category.name)}
+                                        {hasSubCategories && <span className="ml-auto"> &gt; </span>}
+                                    </span>
+                                </Link>
+
+                                {hoveredCategory === category._id && hasSubCategories && (
+                                    <div
+                                        className="absolute top-0 left-full w-56 bg-white shadow-md z-20 ml-1 p-2"
+                                        onMouseEnter={() => handleMouseEnterCategory(category._id)}
+                                        onMouseLeave={handleMouseLeave}
                                     >
-                                        <div className="image">
-                                            <img
-                                                src="https://vistamart.biz/storage/app/public/category/2024-08-08-66b4dde53db3a.png"
-                                                alt="Logo"
-                                                className="w-[1.5vw] h-[1vw] "
-                                            />
-                                        </div>
-                                        <Typography className="text-gray-700 group-hover:text-primary-600">
-                                            {capitalizeFirstLetter(
-                                                category.name
-                                            )}
-                                        </Typography>
-                                    </Link>
-                                </>
-                            )
+                                        {category.subCategories.map((subCategory, subIndex) => {
+                                            const hasSubSubCategories =
+                                                subCategory.subSubCategories?.length > 0;
+
+                                            return (
+                                                <div
+                                                    key={subIndex}
+                                                    className="group relative"
+                                                    onMouseEnter={() =>
+                                                        handleMouseEnterSubCategory(subCategory._id)
+                                                    }
+                                                    onMouseLeave={() => setHoveredSubCategory(null)}
+                                                >
+                                                    <Link
+                                                        to={`/products?subcategory=${subCategory._id}`}
+                                                        className="flex justify-between p-2 hover:bg-gray-100"
+                                                    >
+                                                        <span className="px-5">
+                                                            {capitalizeFirstLetter(
+                                                                subCategory.name
+                                                            )}
+                                                        </span>
+                                                        {hasSubSubCategories && (
+                                                            <span className="ml-auto">▶</span>
+                                                        )}
+                                                    </Link>
+
+                                                    {hoveredSubCategory === subCategory._id &&
+                                                        hasSubSubCategories && (
+                                                            <div className="absolute top-0 left-full w-56 bg-white shadow-md ml-1 z-20 p-2">
+                                                                {subCategory.subSubCategories.map(
+                                                                    (subSubCategory, subSubIndex) => (
+                                                                        <Link
+                                                                            key={subSubIndex}
+                                                                            to={`/products?subsubcategory=${subSubCategory._id}`}
+                                                                            className="block p-2 hover:bg-gray-100"
+                                                                        >
+                                                                            {capitalizeFirstLetter(
+                                                                                subSubCategory.name
+                                                                            )}
+                                                                        </Link>
+                                                                    )
+                                                                )}
+                                                            </div>
+                                                        )}
+                                                </div>
+                                            );
+                                        })}
+                                    </div>
+                                )}
+                            </div>
+                        );
                     })}
+
                     <Link
                         to={`/categories`}
                         className="group text-center cursor-pointer w-full"
                     >
-                        <Typography className="text-primary-500 p-1 group-hover:text-primary-600">
-                            view more
-                        </Typography>
+                        <span className="text-primary-500 p-2 hover:text-primary-600">
+                            View More
+                        </span>
                     </Link>
                 </div>
-            </div>
-        </>
-    ) : (
-        <p>No categories found!</p>
-    )
-}
+            ) : (
+                <p>No categories found!</p>
+            )}
+        </div>
+    );
+};
 
-export default CategoryDropDown
+export default CategoryDropDown;
--- client/src/components/Header/BottomNavbar.jsx
+++ client/src/components/Header/BottomNavbar.jsx
@@ -4,9 +4,6 @@ import {
     Collapse,
     Typography,
     IconButton,
-    Menu,
-    MenuHandler,
-    MenuList,
 } from '@material-tailwind/react'
 import { MdArrowDropDown } from 'react-icons/md'
 import { BiSolidCategory } from 'react-icons/bi'
@@ -33,19 +30,15 @@ const BottomNavbar = () => {
     return (
         <div className="w-full py-3 border-none shadow-none bg-primary-400 hidden md:block ">
             <div className="flex items-center gap-5 mx-16">
-                <div className="w-[250px] bg-white items-center">
-                    <Menu
-                        open={openMenu3}
-                        handler={setOpenMenu3}
-                        allowHover
-                        className="flex flex-col"
-                    >
-                        <MenuHandler>
+                <div className="w-[250px] bg-white items-center relative">
+                 
                             <Typography
                                 as="li"
                                 variant="small"
                                 color="blue-gray"
                                 className="p-2 font-medium"
+                                onMouseOver={()=>setOpenMenu3(true)}
+                               
                             >
                                 <p className="flex items-center hover:text-primary-500 text-primary-400 transition-all duration-300 ease-in justify-between cursor-pointer">
                                     <BiSolidCategory className="w-6 h-6 mr-2" />
@@ -53,15 +46,17 @@ const BottomNavbar = () => {
                                     <MdArrowDropDown className="w-6 h-6 ml-2" />
                                 </p>
                             </Typography>
-                        </MenuHandler>
+                        
                         {window.location.pathname === '/' ? (
                             ''
                         ) : (
-                            <MenuList className="hidden overflow-visible md:block p-0">
-                                <CategoryDropDown />
-                            </MenuList>
+                            openMenu3?
+                            <div className="Box absolute top-[8vh]  "  onMouseLeave={()=>setOpenMenu3(false)}>
+                            <CategoryDropDown  className/>
+                            </div>
+                            :""
                         )}
-                    </Menu>
+                 
                 </div>
 
                 <div className="hidden lg:block">
